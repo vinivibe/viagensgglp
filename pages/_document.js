8b222f6e3063608473241d1826491bf7774d2777
Vini <mvfv@outlook.com>
2024-04-24 02:42:05 -0300

    new commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Link from "next/link";
-import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -12,27 +11,6 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <Script strategy="lazyOnload">
-          {`
-          if (typeof MauticSDKLoaded == 'undefined') {
-            var MauticSDKLoaded = true;
-            var head            = document.getElementsByTagName('head')[0];
-            var script          = document.createElement('script');
-            script.type         = 'text/javascript';
-            script.src          = 'https://mautic.intz.scarf.gg/media/js/mautic-form.js?v42b75280';
-            script.onload       = function() {
-                MauticSDK.onLoad();
-            };
-            head.appendChild(script);
-            var MauticDomain = 'https://mautic.intz.scarf.gg';
-            var MauticLang   = {
-              'submittingMessage': "Por favor, aguarde..."
-            }
-          } else if (typeof MauticSDK != 'undefined') {
-            MauticSDK.onLoad();
-          }
-        `}
-        </Script>
         <body>
           <Main />
           <NextScript />
